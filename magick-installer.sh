103ad1930a119e498fa01530cb5fde7af904d0a4
Dale Suesse <dalesuesse@gmail.com>
2011-04-02 22:35:21 -0500

    Updated to 6.6.9-3

--- magick-installer.sh
+++ magick-installer.sh
@@ -17,9 +17,9 @@ download http://www.ijg.org/files/jpegsrc.v8b.tar.gz
 download http://www.sfr-fresh.com/unix/misc/tiff-3.9.4.tar.gz
 download http://voxel.dl.sourceforge.net/project/wvware/libwmf/0.2.8.4/libwmf-0.2.8.4.tar.gz
 download http://downloads.sourceforge.net/project/lcms/lcms/1.19/lcms-1.19.tar.gz
-download http://heanet.dl.sourceforge.net/project/ghostscript/GPL%20Ghostscript/9.00/ghostscript-9.00.tar.gz
+download http://heanet.dl.sourceforge.net/project/ghostscript/GPL%20Ghostscript/9.02/ghostscript-9.02.tar.gz
 download http://voxel.dl.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz
-download ftp://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/ImageMagick-6.6.7-0.tar.gz
+download ftp://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/ImageMagick-6.6.9-3.tar.gz
 
 tar xzvf freetype-2.4.3.tar.gz
 cd freetype-2.4.3
@@ -76,8 +76,8 @@ sudo make install
 cd ..
 
 
-tar zxvf ghostscript-9.00.tar.gz
-cd ghostscript-9.00
+tar zxvf ghostscript-9.02.tar.gz
+cd ghostscript-9.02
 ./configure  --prefix=/usr/local
 make clean
 make
@@ -90,8 +90,8 @@ sudo mkdir -p /usr/local/share/ghostscript/fonts
 sudo mv -f fonts/* /usr/local/share/ghostscript/fonts
 
 
-tar xzvf ImageMagick-6.6.7-0.tar.gz
-cd ImageMagick-6.6.7-0
+tar xzvf ImageMagick-6.6.9-3.tar.gz
+cd ImageMagick-6.6.9-3
 export CPPFLAGS=-I/usr/local/include
 export LDFLAGS=-L/usr/local/lib
 ./configure --prefix=/usr/local --disable-static --with-modules --without-perl --without-magick-plus-plus --with-quantum-depth=8 --with-gs-font-dir=/usr/local/share/ghostscript/fonts --disable-openmp
